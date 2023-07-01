2b3b27bb0895e85ca9e579b737298c4ddd6b548f
Andrei Piontkovskii <andrei@p10i.ru>
2023-07-01 17:49:25 +0000

    #25 define eslit's react settings in the package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,24 @@
 module.exports = {
     "extends": ["./node_modules/@vngn/codestyle/.eslintrc.cjs"],
+    "overrides": [
+        {
+            "files": ["*.ts", "*.tsx"],
+            "extends": [
+                // Use plugin for tsx files:
+                "plugin:react/recommended",
+            ],
+            "rules": {
+                // Prevent importing the submodules of other modules
+                "import/no-internal-modules": "off",
+                // Forbid importing modules from parent directories
+                "import/no-relative-parent-imports": "off",
+                // Forbid default exports
+                "import/no-default-export": "off",
+                // Forbid namespace (a.k.a. "wildcard" *) imports
+                "import/no-namespace": "off",
+            },
+        },
+    ],
     "parserOptions": {
         // Use tsconfig from root directory:
         "tsconfigRootDir": __dirname,
